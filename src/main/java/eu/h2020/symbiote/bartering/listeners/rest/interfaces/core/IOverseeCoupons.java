6fed17748b389701598f04ade1a2c4e9b991911d
Jakub Toczek <jakub.toczek@man.poznan.pl>
2018-06-18 09:47:58 +0200

    Core communication secured

--- src/main/java/eu/h2020/symbiote/bartering/listeners/rest/interfaces/core/IOverseeCoupons.java
+++ src/main/java/eu/h2020/symbiote/bartering/listeners/rest/interfaces/core/IOverseeCoupons.java
@@ -3,21 +3,29 @@ package eu.h2020.symbiote.bartering.listeners.rest.interfaces.core;
 import eu.h2020.symbiote.security.commons.SecurityConstants;
 import eu.h2020.symbiote.security.communication.payloads.CouponValidity;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @Profile("core")
 public interface IOverseeCoupons {
 
     @PostMapping(value = SecurityConstants.BTM_REGISTER_COUPON)
-    ResponseEntity<String> registerCoupon(@RequestBody String couponString);
+    ResponseEntity<String> registerCoupon(
+            @RequestHeader HttpHeaders httpHeaders,
+            @RequestBody String couponString);
 
     @PostMapping(value = SecurityConstants.BTM_IS_COUPON_VALID)
-    ResponseEntity<CouponValidity> isCouponValid(@RequestBody String couponString);
+    ResponseEntity<CouponValidity> isCouponValid(
+            @RequestHeader HttpHeaders httpHeaders,
+            @RequestBody String couponString);
 
     @PostMapping(value = SecurityConstants.BTM_CONSUME_COUPON)
-    ResponseEntity<String> consumeCoupon(@RequestBody String couponString);
+    ResponseEntity<String> consumeCoupon(
+            @RequestHeader HttpHeaders httpHeaders,
+            @RequestBody String couponString);
 
     @PostMapping(value = SecurityConstants.BTM_CLEANUP_COUPONS)
     ResponseEntity<Integer> cleanupConsumedCoupons(@RequestBody long timestamp);
