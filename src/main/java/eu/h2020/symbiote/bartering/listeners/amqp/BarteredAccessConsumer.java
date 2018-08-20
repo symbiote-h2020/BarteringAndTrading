1bd40c22d0bae1e9175a58b39d131852b79c539b
vthglyk <vasgl@intracom-telecom.com>
2018-08-20 11:11:31 +0300

    Fixed BarteredAccessConsumer

--- src/main/java/eu/h2020/symbiote/bartering/listeners/amqp/BarteredAccessConsumer.java
+++ src/main/java/eu/h2020/symbiote/bartering/listeners/amqp/BarteredAccessConsumer.java
@@ -2,28 +2,15 @@ package eu.h2020.symbiote.bartering.listeners.amqp;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.h2020.symbiote.bartering.services.BarteredAccessManagementService;
-import eu.h2020.symbiote.security.commons.exceptions.custom.BTMException;
-import eu.h2020.symbiote.security.commons.exceptions.custom.InvalidArgumentsException;
-import eu.h2020.symbiote.security.commons.exceptions.custom.SecurityHandlerException;
-import eu.h2020.symbiote.security.commons.exceptions.custom.ValidationException;
-import eu.h2020.symbiote.security.commons.exceptions.custom.WrongCredentialsException;
+import eu.h2020.symbiote.security.commons.exceptions.custom.*;
 import eu.h2020.symbiote.security.communication.payloads.BarteredAccessRequest;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.amqp.AmqpException;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessagePostProcessor;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.support.AmqpHeaders;
+import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
-import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -50,13 +37,12 @@ public class BarteredAccessConsumer {
 					internal = "${rabbit.exchange.aam.internal}",
 					type = "topic"),
 			key = "${rabbit.routingKey.bartered.access}"))
-	public void authorizeBarteredAccess(String message, @Header(AmqpHeaders.REPLY_TO) String replyTo,
-			@Header(AmqpHeaders.CORRELATION_ID) String correlationId) {
+	public String authorizeBarteredAccess(String message) {
 
-		String result ="";
+		String result;
 		try {
 			ObjectMapper om = new ObjectMapper();
-			BarteredAccessRequest barteredAccessRequest = null;
+			BarteredAccessRequest barteredAccessRequest;
 			barteredAccessRequest = om.readValue(message, BarteredAccessRequest.class);
 
 
@@ -89,15 +75,7 @@ public class BarteredAccessConsumer {
 			log.error(result);
 		}
 
-		amqpTemplate.convertAndSend(replyTo, (Object)result, new MessagePostProcessor() {
-			@Override
-			public Message postProcessMessage(Message message) throws AmqpException {
-				message.getMessageProperties().setCorrelationIdString(correlationId);
-				return message;
-			}
-		});
-
-
+		return result;
 	}
 
 }
